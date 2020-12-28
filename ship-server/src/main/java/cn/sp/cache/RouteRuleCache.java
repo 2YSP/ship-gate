b43b5a491030eddf35e3533cd7db47b3e6f7aa97
ship <1123804727@qq.com>
2020-12-28 22:32:31 +0800

    feat: remove rule from cache map

--- ship-server/src/main/java/cn/sp/cache/RouteRuleCache.java
+++ ship-server/src/main/java/cn/sp/cache/RouteRuleCache.java
@@ -2,11 +2,14 @@ package cn.sp.cache;
 
 import cn.sp.exception.ShipException;
 import cn.sp.pojo.dto.AppRuleDTO;
+import org.springframework.util.CollectionUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 /**
  * @Author: Ship
@@ -20,12 +23,34 @@ public class RouteRuleCache {
      */
     private static final Map<String, List<AppRuleDTO>> ROUTE_RULE_MAP = new ConcurrentHashMap<>();
 
+    /**
+     * add rule to cache map
+     *
+     * @param map
+     */
     public static void add(Map<String, List<AppRuleDTO>> map) {
         ROUTE_RULE_MAP.putAll(map);
     }
 
+    /**
+     * remove rule from cache map
+     *
+     * @param map
+     */
     public static void remove(Map<String, List<AppRuleDTO>> map) {
-        // todo
+        for (Map.Entry<String, List<AppRuleDTO>> entry : map.entrySet()) {
+            String appName = entry.getKey();
+            List<Integer> ruleIds = entry.getValue().stream().map(AppRuleDTO::getId).collect(Collectors.toList());
+            List<AppRuleDTO> ruleDTOS = ROUTE_RULE_MAP.getOrDefault(appName, new ArrayList<>());
+            List<AppRuleDTO> leftRules = ruleDTOS.stream().filter(r -> !ruleIds.contains(r.getId())).collect(Collectors.toList());
+            if (CollectionUtils.isEmpty(leftRules)) {
+                // remove all
+                ROUTE_RULE_MAP.remove(appName);
+            } else {
+                // remove some of them
+                ROUTE_RULE_MAP.put(appName, leftRules);
+            }
+        }
     }
 
 
